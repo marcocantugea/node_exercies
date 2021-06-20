75727e455d706bf722d5a41a0cbb1c15e6ba82ab
Marco Cantu Gea <marco.cantu.g@gmail.com>
2021-06-20 08:16:54 -0500

    more examples

---
+++ examples/node_underscore_functions.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * clase de underscore que tiene funciones muy usadas 
+ * 
+ */
+
+const underscore= require("underscore");
+
+
+result = underscore.contains([1,2,3,4,5,6,3],5);
+
+console.log(result);
+
+result_s=underscore.isNumber("e");
+console.log(result_s);
